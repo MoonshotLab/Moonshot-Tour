77c744a629ea262eed2c28054ea18d2a9245c9ff
Joe Longstreet <joelongstreet@gmail.com>
2015-04-01 08:36:04 -0500

    populate content, support different media types

--- db.js
+++ db.js
@@ -3,29 +3,79 @@ module.exports = function(){
     {
       pin   : 2,
       data  : {
-        title     : 'Wall of Rock: US Air Guitar Championships',
-        youtubeId : '7lncfxYM4VM'
+        title     : 'Market Watch',
+        img       : 'https://camo.githubusercontent.com/7c9bc7b250fc529131530882caf425364c93a2cd/687474703a2f2f692e696d6775722e636f6d2f754a36517438422e676966'
       }
     },
     {
       pin   : 3,
       data  : {
-        title     : 'Beacon Hack Day at Moonshot',
-        youtubeId : '0132oAUf7E4'
+        title     : 'Frost Bite',
+        vid       : '/frostbite.mov'
       }
     },
     {
       pin   : 4,
       data  : {
-        title     : 'Moonshot Rotation Week - Barkley Innovation Lab',
-        youtubeId : '980Zi56U-E0'
+        title     : 'Beacon Hack Day',
+        youtubeId : '0132oAUf7E4'
       }
     },
     {
       pin   : 5,
       data  : {
-        title     : 'Bang'
+        title     : 'Cocktails and Code',
+        img       : '/img/Cocktails_Monitor_Stills.jpg'
       }
     },
+    {
+      pin   : 6,
+      data  : {
+        title     : 'Feel Think Make',
+        img       : '/img/FTM_Monitor_Stills.jpg'
+      }
+    },
+    {
+      pin   : 7,
+      data  : {
+        title     : 'Moonshot',
+        youtubeId : 'K6hhZTlQegM'
+      }
+    },
+    {
+      pin   : 8,
+      data  : {
+        title     : 'Internet of Wild Things',
+        img       : '/img/IOT_Monitor_Stills.jpg'
+      }
+    },
+    {
+      pin   : 9,
+      data  : {
+        title     : 'Rotation Week',
+        youtubeId : '980Zi56U-E0'
+      }
+    },
+    {
+      pin   : 10,
+      data  : {
+        title     : '3d Printing',
+        img       : '/img/3D_Monitor_Stills.jpg'
+      }
+    },
+    {
+      pin   : 11,
+      data  : {
+        title     : 'Hot Light',
+        vid       : '/hotlite.m4v'
+      }
+    },
+    {
+      pin   : 12,
+      data  : {
+        title     : 'Wall of Rock',
+        youtubeId : '7lncfxYM4VM'
+      }
+    }
   ];
 };
--- public/index.js
+++ public/index.js
@@ -3,8 +3,29 @@ var bodyWidth = $('body').width();
 
 
 socket.on('tap', function(e){
+  $('#player-container').html('');
+
   if(e.data.youtubeId) loadYoutubeVideo(e.data.youtubeId);
+  else if(e.data.vid) makeVideo(e.data.vid);
+  else if(e.data.img) loadImg(e.data.img);
 });
+
+
+function loadImg(img){
+  var template = '<img src="' + img + '" />';
+  $('#player-container').html(template);
+}
+
+
+function makeVideo(url){
+  var template = [
+    '<video width="' + bodyWidth + '" autoplay=true>',
+      '<source type="video/mp4" src="' + url + '"/>',
+    '</video>'
+  ].join();
+
+  $('#player-container').html(template);
+}
 
 
 function loadYoutubeVideo(shortCode){
