343f8d556e43e8ea43a979c1f42453e4a42e9f70
Joe Longstreet <joelongstreet@gmail.com>
2015-04-01 08:38:53 -0500

    reverse order of pin and value, dumber touch detection logic

--- CapacitiveSensor/CapacitiveSensor.ino
+++ CapacitiveSensor/CapacitiveSensor.ino
@@ -2,34 +2,43 @@
 
 
 CapacitiveSensor proximitySensors[] = {
-  CapacitiveSensor(2,3),
-  CapacitiveSensor(4,5)
+  CapacitiveSensor(2,42),
+  CapacitiveSensor(3,43),
+  CapacitiveSensor(4,44),
+  CapacitiveSensor(5,45),
+  CapacitiveSensor(6,46),
+  CapacitiveSensor(7,47),
+  CapacitiveSensor(8,48),
+  CapacitiveSensor(9,49),
+  CapacitiveSensor(10,50),
+  CapacitiveSensor(11,51),
+  CapacitiveSensor(12,52)
 };
 
 
 void setup()
 {
   Serial.begin(9600);
-  
-  // use a 3 second calibration
-  for(int i=0; i<2; i++){
-    proximitySensors[i].set_CS_AutocaL_Millis(0xFFFFFFFF);
-  } 
+
+  // use a calibration
+  for(int i=0; i<11; i++){
+    proximitySensors[i].set_CS_AutocaL_Millis(2000);
+  }
 }
 
 
-void loop()                    
+void loop()
 {
   // loop over each sensor, printing the sensor values as well
   // as the pin numbers. The pin number has 2 added because we're using
   // the node server to identify pins by their receive pin
-  for(int i=0; i<2; i++){
+  for(int i=0; i<11; i++){
     long proximity = proximitySensors[i].capacitiveSensor(30);
-    Serial.print(proximity);
-    Serial.print("-");
     Serial.print(i+2);
+    Serial.print("-");
+    Serial.print(proximity);
     Serial.println();
-  } 
+  }
 
-  delay(100);
+  delay(50);
 }
